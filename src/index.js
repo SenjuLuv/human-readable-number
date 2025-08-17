60360e4e91dfdb5ac7aa03dfd678791fd762e469
Roman Slesareff <roman.slesareff@gmail.com>
2025-08-17 16:59:45 +0300

    fix index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-module.exports = function toReadable(/* number */) {
-  throw new Error('Not implemented');
-};
+module.exports = function toReadable(num) {
+  const number = [
+    "zero" , "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven",
+    "twelve", "thirteen", "fourteen", "fifteen","sixteen", "seventeen","eighteen", "nineteen"
+  ];
+  const tens = [
+    "", "", "twenty","thirty","forty","fifty","sixty","seventy","eighty", "ninety"
+  ];
+  if (num < 20) {
+    return number[num];
+  } else if ( num < 100) {
+    const tenp = Math.floor( num / 10);
+    const unitp = num % 10;
+    return tens[tenp] + (unitp ? "" + number[unitp] : "");
+  } else if (num < 1000) {
+    const hundredp = Math.floor(num / 100);
+        const rest = num % 100;
+        return number[hundredp] + " hundred" + (rest ? " " + numberToWords(rest) : "");
+    }
+  }
