a65f196002bac21c93f2198c811750f957d289e3
Roman Slesareff <roman.slesareff@gmail.com>
2025-08-17 17:14:44 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,57 @@
 module.exports = function toReadable(num) {
   const number = [
-    "zero" , "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven",
-    "twelve", "thirteen", "fourteen", "fifteen","sixteen", "seventeen","eighteen", "nineteen"
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen',
   ];
+
   const tens = [
-    "", "", "twenty","thirty","forty","fifty","sixty","seventy","eighty", "ninety"
+    '',
+    '',
+    'twenty',
+    'thirty',
+    'forty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety',
   ];
+
   if (num < 20) {
     return number[num];
-  } else if ( num < 100) {
-    const tenp = Math.floor( num / 10);
-    const unitp = num % 10;
-    return tens[tenp] + (unitp ? "" + number[unitp] : "");
-  } else if (num < 1000) {
-    const hundredp = Math.floor(num / 100);
-        const rest = num % 100;
-        return number[hundredp] + " hundred" + (rest ? " " + numberToWords(rest) : "");
-    }
   }
+  if (num < 100) {
+    const tenPart = Math.floor(num / 10);
+    const unitPart = num % 10;
+    return `${tens[tenPart]}${unitPart ? ` ${number[unitPart]}` : ''}`;
+  }
+  if (num < 1000) {
+    const hundredPart = Math.floor(num / 100);
+    const rest = num % 100;
+    return `${number[hundredPart]} hundred${rest ? ` ${toReadable(rest)}` : ''}`;
+  }
+  if (num < 10000) {
+    const thousandPart = Math.floor(num / 1000);
+    const rest = num % 1000;
+    return `${number[thousandPart]} thousand${rest ? ` ${toReadable(rest)}` : ''}`;
+  }
+  return 'Number out of range';
+};
